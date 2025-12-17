9c58f49eeaae1f0c2bfd9ea082d6eb7311abbe2f
amirshah07 <144581429+amirshah07@users.noreply.github.com>
2025-12-17 21:16:50 +0800

    set up PostgreSQL database with star schema. 3 tables created: coins (dimension), time_intervals (dimension), crypto_prices (fact). Scehma auto-executes on container startup

---
+++ sql/create_tables.sql
@@ -0,0 +1,33 @@
+CREATE TABLE IF NOT EXISTS coins (
+    coin_key SERIAL PRIMARY KEY,
+    coin_id VARCHAR NOT NULL UNIQUE, -- 'bitcoin'
+    symbol VARCHAR NOT NULL, -- 'btc'
+    name VARCHAR NOT NULL, -- 'Bitcoin'
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS time_intervals (
+    time_key INTEGER PRIMARY KEY, -- YYYYMMDDHHMM
+    timestamp TIMESTAMP NOT NULL,
+    date DATE NOT NULL,
+    hour INTEGER NOT NULL,
+    minute INTEGER NOT NULL,
+    day INTEGER NOT NULL,
+    month INTEGER NOT NULL,
+    year INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS crypto_prices (
+    price_id SERIAL PRIMARY KEY,
+    coin_key INTEGER NOT NULL,
+    time_key INTEGER NOT NULL,
+    price_usd NUMERIC NOT NULL,
+    market_cap NUMERIC,
+    volume_24h NUMERIC,
+    price_change_pct_24h NUMERIC,
+    collected_at TIMESTAMP NOT NULL,
+
+    FOREIGN KEY (coin_key) REFERENCES coins(coin_key),
+    FOREIGN KEY (time_key) REFERENCES time_intervals(time_key)
+);
